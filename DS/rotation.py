f79ae9ff70c2741d7373ea9795918a0ddd3453b9
yashrajsinhjadej <127673916+yashrajsinhjadej@users.noreply.github.com>
2025-01-07 17:14:03 +0530

    Add files via upload

---
+++ DS/rotation.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        else:
+            return -1 
